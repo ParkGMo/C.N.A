40d3b9741b0946d1993b65c642000e5a3587977f
ParkGMo <167054605+ParkGMo@users.noreply.github.com>
2024-10-28 15:43:34 +0900

    10.28.15:43

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import CountryInfo from './pages/countryInfo/CountryInfo';
-import Intro from './pages/intro/Intro';
-import './scss/global.scss';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import CountryInfo from "./pages/countryInfo/CountryInfo";
+import Intro from "./pages/intro/Intro";
+import "./scss/global.scss";
 
 function App() {
   return (
---
+++ src/components/exchangeRate/ExchageRate.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import styles from "./ExchageRate.scss";
+
+function ExchageRate() {
+  const [exchangeRateList, setExchangeRateList] = useState([]);
+  const handleLoad = async () => {
+    const country = "kr";
+    const language = "ko";
+    const url = `https://real-time-finance-data.p.rapidapi.com/market-trends?trend_type=CURRENCIES&country=${country}&language=${language}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "23eeafc821msh7be001b9cf669b8p1fea80jsn801714754628",
+        "x-rapidapi-host": "real-time-finance-data.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setExchangeRateList(result?.data?.trends);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    handleLoad();
+  }, []);
+  return (
+    <div className={styles.ExchangeRate}>
+      <div className={styles.ExchangeRateTitle}>환율</div>
+      <div className={styles.ExchangeRateBoxes}>
+        {exchangeRateList?.map((item) => {
+          const formSymbol = item.from_symbol;
+          const fromName = item.from_currency_name;
+          const toSymbol = item.to_symbol;
+          const toName = item.to_currency_name;
+          const update = item.last_update_utc;
+          const price = item.previous_close;
+          return (
+            <div className={styles.ExchangeRateBox}>
+              <div className={styles.ExchangeRateFromBox}>
+                1 {`${formSymbol}`}
+                <span className={styles.ExchangeRateName}>
+                  ({`${fromName}`})
+                </span>
+              </div>
+              <div className={styles.ExchangeRateToBox}></div>
+              {`${price}`}
+              {`${toSymbol}`}
+              <span className={styles.ExchangeRateName}>({`${toName}`})</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ExchageRate;
